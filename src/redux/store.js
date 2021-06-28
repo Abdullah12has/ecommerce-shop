d5aa9aa4c3ef87309e2bae53479a6ac7c514119c
Syed Abdullah Hassan <abdullah.hassan@ug.bilkent.edu.tr>
2021-06-28 03:38:11 +0200

    small change

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,18 +3,16 @@ import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import rootReducer from "./rootReducer";
 
-const persistedState = localStorage.getItem('reduxState')
-  ? JSON.parse(localStorage.getItem('reduxState'))
-  : {user:{}, cart:{hidden:Boolean,cartItems:[]}};
+const state = localStorage.getItem("reduxState")
+  ? JSON.parse(localStorage.getItem("reduxState"))
+  : { user: {}, cart: { hidden: Boolean, cartItems: [] } };
 
-const middlewares = persistedState;
-
+const middlewares = state;
 
 const store = createStore(rootReducer, middlewares);
 
 store.subscribe(() => {
-    localStorage.setItem('reduxState', JSON.stringify(store.getState()))
-  })
-
+  localStorage.setItem("reduxState", JSON.stringify(store.getState()));
+});
 
 export default store;
