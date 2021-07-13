0551450e1bd068183140f53f87c895472f719630
Syed Abdullah Hassan <abdullah.hassan@ug.bilkent.edu.tr>
2021-07-14 00:51:43 +0200

    matrices added

--- src/App.js
+++ src/App.js
@@ -10,12 +10,17 @@ import { setUser } from "./redux/userActions";
 import Hats from "./screens/Hats";
 import Checkout from "./screens/Checkout";
 
-
+// SOLID_________
 // follows single responsibility principle each component is has a single functionality Reducers, actions, UI components 
 // objects like React.Component, screens/hats are open for extension but closed for modification
 // follows Liskov Substitution Principle. child classes can replace the parent classes like any can replace react.Component because their the children classes 
 // Interface Segregation Principle is followed because you can use most of the components without importing any other functions/components that are used with them. And all unused components are not imported inside components 
 // dependency inversion principle is followed because it doesn't matter to the functions and components how the data is being send to them by parents and what's going on inside other functions 
+
+// Functional Programming 
+// converted class components to functional components some screens (others are difficult to convert so didn't convert all of them), used maps, filter, and consts instead of let. Reducers make a new object on change 
+
+
 
 
 
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import {getCLS, getFID, getLCP, getTTFB, getFCP} from 'web-vitals';
 
 ReactDOM.render(
   <Provider store={store}>
@@ -19,3 +20,10 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals(console.log);
+
+getCLS(console.log); //Cumulative Layout Shift < 0.1
+getFID(console.log); //  First Input Delay < 100ms
+getLCP(console.log);// Largest Contentful Paint < 2.5 sec
+getFCP(console.log);//First Contentful Paint
+getTTFB(console.log);// Reduce server response times
+// https://web.dev/vitals/ for more info about these vitals
